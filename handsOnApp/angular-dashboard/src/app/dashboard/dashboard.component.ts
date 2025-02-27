c51a493fc4bcb524bb15c52adf73dcee97225174
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-02-27 12:48:55 +0500

    dashboard components

--- handsOnApp/angular-dashboard/src/app/dashboard/dashboard.component.ts
+++ handsOnApp/angular-dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgChartsModule } from 'ng2-charts';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, NgChartsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  analyticsData: any;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.fetchAnalyticsData();
+  }
+
+  fetchAnalyticsData(): void {
+    this.apiService.getAnalyticsData().subscribe(data => {
+      this.analyticsData = data;
+    });
+  }
 }
