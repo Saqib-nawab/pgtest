f9095cfd6f2034c20381d2c668233995b1bf7629
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-02-27 13:52:10 +0500

    we will find it on our way through

---
+++ DeploymentStrategy(section3).js
@@ -0,0 +1,11 @@
+// Deployment Strategy(10 points)
+// o How would you deploy the entire platform, including ML components, on a
+// cloud service(AWS / Azure / GCP) ?
+
+
+// 1. Infrastructure as Code (IaC) with Terraform: Use Terraform to define your infrastructure, including the ML components. Terraform allows you to define your infrastructure in a declarative way, making it easier to manage and version your infrastructure.
+
+// 2. Mention the services you would use, how you would manage scaling, and
+// how you would handle downtime.
+
+// 3. Use AWS Elastic Beanstalk for deploying the web application. Elastic Beanstalk is a Platform as a Service (PaaS) that allows you to easily deploy and manage web applications.
